0fe4e8b5e86a1aa5c46f1db7ed0cba658e54d896
choclitoxd <141989583+choclitoxd@users.noreply.github.com>
2024-08-09 15:06:54 -0500

    view Controller

--- co.edu.uniquindio.CitasHospital/src/main/java/co/edu/uniquindio/citashospital/citashospital/viewController/CitasViewController.java
+++ co.edu.uniquindio.CitasHospital/src/main/java/co/edu/uniquindio/citashospital/citashospital/viewController/CitasViewController.java
@@ -1,7 +1,38 @@
 package co.edu.uniquindio.citashospital.citashospital.viewController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class CitasViewController {
+    @FXML
+    private TableColumn<?, ?> colCodigoCita;
 
+    @FXML
+    private TableColumn<?, ?> colDate;
 
+    @FXML
+    private TableColumn<?, ?> colDoctor;
 
+    @FXML
+    private TableColumn<?, ?> colPaciente;
+
+    @FXML
+    private DatePicker dateCita;
+
+    @FXML
+    private MenuButton ddlDoctor;
+
+    @FXML
+    private MenuButton ddlPaciente;
+
+    @FXML
+    private TableView<?> tableCita;
+
+    @FXML
+    void onAddCita(ActionEvent event) {
+
+    }
 }
